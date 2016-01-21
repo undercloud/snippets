1505a11818884c39e21563dcb9bd457da2fd3ddb
undercloud <alexandrostruses@yandex.ru>
2016-01-21 12:14:58 +0400

    Update array_bound.php

--- php/array/array_bound.php
+++ php/array/array_bound.php
@@ -1,14 +1,13 @@
 <?php
-	
+
 	if (false == function_exists('array_bound')) {
-		function array_bound(array $array, array $keys = array()) {
+		function array_bound(array $array, array $keys = array(), $reverse = false) {
 			$bound = array();
 			foreach($array as $key => $value){
-				if(in_array($key,$keys)) {
+				if($reverse != in_array($key,$keys)) {
 					$bound[$key] = $value;
 				}
 			}
-
 			return $bound;
 		}
 	}
